df7579e92cf8e3f40d597ff8a7901f8911760efd
Nelson Amador <rod6214@gmail.com>
2024-09-09 22:11:17 -0300

    Initial commit

---
+++ init.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=%PATH%;C:\Develop\bin\arm-gnu-toolchain-13.3.rel1-mingw-w64-i686-arm-none-eabi\arm-gnu-toolchain-13.3.rel1-mingw-w64-i686-arm-none-eabi\bin;C:\Develop\bin\make-3.81-bin\bin;C:\Develop\RaspberryProjects\pico-sdk;
+set PICO_SDK_PATH=C:\Develop\RaspberryProjects\pico-sdk
